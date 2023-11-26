32f1f10292b5f58b9e3b82f8eaffeea005ce131b
Maori-boi <micah_pee@student.asdah.school.nz>
2023-11-26 14:46:00 +1300

    Made Changes to the game over scene

---
+++ game_over.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Utils.saveGame()
+	Utils.loadGame()
+
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+func _on_main_menu_pressed():
+	get_tree().change_scene_to_file("res://main.tscn")
+
+
+func _on_credits_pressed():
+	get_tree().change_scene_to_file("res://credits.tscn")
